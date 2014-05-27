51fec6a6e4eac684d598f29292fb2dcbe23e414d
prad178 <pradeep.pradmail@gmail.com>
2014-05-27 10:53:01 -0400

    Router module added

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.write("1 - 1 is " + (1-1));
-  response.end();
-}).listen(8887);
+function start(route){
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request received for " + pathname + ".");
+        
+        route(pathname);
+        
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.write("Hello World!");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8887);
+    console.log("Server has started");
+}
+
+exports.start = start;
